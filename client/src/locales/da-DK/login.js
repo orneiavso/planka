b69b261010ea809402308820b2a3dc461f16b9d0
Orne Iasson <orneiavso@gmail.com>
2026-09-16 16:36:33 +0200

    Import upstream PLANKA v1.26.3 (AGPL-3.0)

---
+++ client/src/locales/da-DK/login.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    common: {
+      emailOrUsername: 'E-mail eller brugernavn',
+      invalidEmailOrUsername: 'Ugyldig e-mail eller brugernavn',
+      invalidPassword: 'Ugyldig adgangskode',
+      logInToPlanka: 'Log på Planka',
+      noInternetConnection: 'Ingen forbindelse til internettet',
+      pageNotFound_title: 'Side ikke fundet',
+      password: 'Adgangskode',
+      projectManagement: 'Projektstyring',
+      serverConnectionFailed: 'Ingen forbindelse til serveren',
+      unknownError: 'Ukendt fejl - prøv igen',
+    },
+
+    action: {
+      logIn: 'Log på',
+    },
+  },
+};
---
+++ client/src/sagas/login/services/router.js
@@ -0,0 +1,67 @@
+import { call, put, select, take } from 'redux-saga/effects';
+import { push } from '../../../lib/redux-router';
+
+import { authenticateUsingOidc, authenticateUsingOidcCallback } from './login';
+import selectors from '../../../selectors';
+import ActionTypes from '../../../constants/ActionTypes';
+import Paths from '../../../constants/Paths';
+
+export function* goToLogin() {
+  yield put(push(Paths.LOGIN));
+}
+
+export function* goToRoot() {
+  yield put(push(Paths.ROOT));
+}
+
+export function* handleLocationChange() {
+  const pathsMatch = yield select(selectors.selectPathsMatch);
+
+  if (!pathsMatch) {
+    return;
+  }
+
+  switch (pathsMatch.pattern.path) {
+    case Paths.ROOT:
+    case Paths.PROJECTS:
+    case Paths.BOARDS:
+    case Paths.CARDS:
+      yield call(goToLogin);
+
+      break;
+    default:
+  }
+
+  const isInitializing = yield select(selectors.selectIsInitializing);
+
+  if (isInitializing) {
+    yield take(ActionTypes.LOGIN_INITIALIZE);
+  }
+
+  switch (pathsMatch.pattern.path) {
+    case Paths.LOGIN: {
+      const oidcConfig = yield select(selectors.selectOidcConfig);
+
+      if (oidcConfig) {
+        const params = new URLSearchParams(window.location.search);
+
+        if (params.has('authenticateWithOidc')) {
+          yield call(authenticateUsingOidc);
+        }
+      }
+
+      break;
+    }
+    case Paths.OIDC_CALLBACK:
+      yield call(authenticateUsingOidcCallback);
+
+      break;
+    default:
+  }
+}
+
+export default {
+  goToLogin,
+  goToRoot,
+  handleLocationChange,
+};
